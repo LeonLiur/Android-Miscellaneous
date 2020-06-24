5a6cb7cd799efe0b192d571586c403ddb8291a27
Leo Liu <leoliuhy@gmail.com>
2020-06-24 17:07:12 -0400

    Added activity Clicker

--- app/src/main/java/com/example/spamclickr/Clicker.java
+++ app/src/main/java/com/example/spamclickr/Clicker.java
@@ -3,12 +3,24 @@ package com.example.spamclickr;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Clicker extends AppCompatActivity {
+
+    private TextView clickCounter;
+    private TextView showTime;
+    private Button clicker;
+    private Button reset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clicker);
+
+        clickCounter = (TextView) findViewById(R.id.clickCounter);
+        showTime = (TextView) findViewById(R.id.showTime);
+        clicker = (Button) findViewById(R.id.buttonClick);
+        reset = (Button) findViewById(R.id.)
     }
 }
